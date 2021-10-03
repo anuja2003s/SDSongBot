1ddbd8aee3ee2bf81be103fba8a16e5c9bbf93c1
anuja2003s <91592406+anuja2003s@users.noreply.github.com>
2021-10-03 18:13:57 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**😍 Uploaded by @Musicworldanu 😍**'
+        rep = '**😍 Uploaded by 🎧MUSIC & VIDEO W🌍RLD™🎧💞 🌀.චැනල් එකට Join වෙන්න:- @Musicworldanu 😍**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
