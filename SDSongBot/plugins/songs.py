949c5a8259f1fef522318f1e53d06ad185bee700
anuja2003s <91592406+anuja2003s@users.noreply.github.com>
2021-12-01 19:15:03 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎙 Uploaded by Me 😇**'
+        rep = '**😍 [{}](tg://user?id={}),\nYour Song🎙 Uploaded by Me 😇**'
     
         
         secmul, dur, dur_arr = 1, 0, duration.split(':')
